3f8c18826e48af5dff13d5932ca2314fe0a1170e
GuillaumeS <guillaume.simler@gmail.com>
2016-04-08 13:45:59 +0200

    Let's start

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//Build a function to count the number of clicks
+
+
+var i = 0;
+
+$('#target').click(function(e) {
+
+	$('#counter').remove();
+	
+	i++;
+
+	$('body').append('<div id="counter">You clicked this cat '+ i +' time(s)</div>')
+})
