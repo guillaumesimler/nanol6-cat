ce33c75395687f3a776bd4bf8c1de82bd6a37f1c
GuillaumeS <guillaume.simler@gmail.com>
2016-04-08 13:51:06 +0200

    Small improvements in JS

--- js/main.js
+++ js/main.js
@@ -6,8 +6,6 @@ var i = 0;
 $('#target').click(function(e) {
 
 	$('#counter').remove();
-	
 	i++;
-
-	$('body').append('<div id="counter">You clicked this cat '+ i +' time(s)</div>')
-})
+	$('body').append('<div id="counter">You clicked this cat '+ i +' time(s)</div>');
+});
