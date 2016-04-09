1a2cab4b322e3dd200c2ada927d574b4e65dfaea
GuillaumeS <guillaume.simler@gmail.com>
2016-04-09 13:12:12 +0200

    Move to premium

--- js/main.js
+++ js/main.js
@@ -19,69 +19,99 @@ var CatName = [
 	];
 
 
+var Cataction = CatActions();
+	
+
 var CatScore = CatScores();
+
+var ClickStart = false;
 
 	// Gunction to create the CatScore
 function CatScores () {
-	Catnb = []
+	var Catnb = [];
 	for (var i = 0; i < CatImg.length; i++) {
 		Catnb.push(0);
 	}
-	return Catnb
+	return Catnb;
+};
+
+function CatActions() {
+	var Catnb = [];
+	for (var i = 0; i < CatImg.length; i++) {
+		Catnb.push($('#Cat' +i));
+	}
+	return Catnb;
 };
 
 // Function to initialize the List of casts
 
 function CatInitialize() {
 	for (var i=0; i<CatImg.length;i++){
-		$('#cat-list').append('<br><li id="cat'+ i +'">The cat n°' + i + ' is called ' + CatName[i] + ' and clicked <span id ="cat-score' + i +'">' +CatScore[i] + '</span> time(s)</li>');
-	}	
+		$('#cat-list').append('<br> <li id="cat'+ i +'">The cat n°' + i + ' is called ' + CatName[i] + ' and clicked ' +CatScore[i] + ' time(s)</li>');
+	}
+	ClickStart = true;
 };
 
 
-
+// Update the score of the single cats
 
 function CatUpdate(counter) {
-	selector = $('#cat-score' + counter);
 
 	var Cat = counter % CatImg.length; 
- 	CatScore[Cat]++;
+	var $selector = $('#cat' + Cat);
 
- 	selector.text(CatScore[Cat]);	
+ 	CatScore[Cat] = CatScore[Cat]+ 1;
+
+ 	$selector.text('The cat n°' + Cat + ' is called ' + CatName[Cat] + ' and clicked ' + CatScore[Cat] + ' time(s)');	
 }
+
 
 // Simple function to add the TOTAL number of clicks 
 function ClickCounter(counter) {
+	//Udpate the image
+	CatDisplay(counter);
+
 	counter++;
 	$('#counter').text('You clicked the cats '+ counter+' time(s)');
 
-	//Udpate the image
-	CatDisplay(counter);
+	
 	return counter;
 }
 
 
-
+// Enables to load the image
 function CatDisplay(counter) {
 	var Cat = counter % CatImg.length; 
 
 	$('#target').attr('src', CatImg[Cat]);
 	$('#catheader').text("Let's click on this beautiful cat, " + CatName[Cat] +'!');
 
-	CatUpdate(counter);
+	// Update the value
+	if (ClickStart) {
+		CatUpdate(counter);
+	}
 }
 
 
+// Starting the 
 function CatCliker() {
 	var i = 0;
-
-	CatInitialize();
+	
 	CatDisplay(i); 
-
+	CatInitialize();
+	
 	$('#target').click(function(e) {
 		i = ClickCounter(i);
-	} );
+	});
 
+	Cataction.forEach(function(cat) {
+		console.log(cat);
+		cat.click(function(e){
+			console.log(e);
+		});
+	});
+	
+	
 }
 	
 // Run the progamm
